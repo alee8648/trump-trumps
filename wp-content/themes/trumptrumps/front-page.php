4c7c8c46bf544051188b31b3c93d308a072816f6
Lee, Adam <Adam.Lee1@edelman.com>
2018-01-25 17:48:22 +0000

    Output the cards on the front page for testing

--- wp-content/themes/trumptrumps/front-page.php
+++ wp-content/themes/trumptrumps/front-page.php
@@ -14,41 +14,41 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+<div id="trump-cards">
+	<?php 
+	$trumps_query = new WP_Query( array(
+		'post_type' 	=>	'trump_card',
+		'post_status'	=>	'publish'
+	));
+	 ?>
+	<?php foreach($trumps_query->posts as $post) {
+		$tweet_content = $post->post_content;
+		$retweet_count = get_field('retweet_count', $post);
+		$comment_count = get_field('comment_count', $post);
+		$like_count = get_field('like_count', $post);
+		$tweet_url = get_field('tweet_url', $post);
+		$tweet_date = get_field('tweet_date', $post)
 
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
+		?>
 
+		<blockquote id="tweet-<?php the_ID(); ?>" class="trump-card twitter-tweet">
+			<header class="tweet-header">
+			</header>
+			<p class="tweet-content">
+				<?php echo $tweet_content; ?>
+				
+
+			</p>
+			<footer class="tweet-footer">
+				Retweets: <?php echo $retweet_count; ?>
+				Comments: <?php echo $comment_count; ?>
+				Likes: <?php echo $like_count; ?>
+				Date: <a href="<?php echo $tweet_url; ?>"><?php echo $tweet_date; ?></a>
+			</footer>
+		</blockquote><!-- #post-## -->
 		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
+	} ?>
+</div>
 
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param int $num_sections Number of front page sections.
-			 */
-			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
-			global $twentyseventeencounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$twentyseventeencounter = $i;
-				twentyseventeen_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
 
 <?php get_footer();
